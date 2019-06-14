8f8c6ff8ed1b4cbc74d2a1b9e8270b5e5bb061f0
Chuludcjr <51537607+Chuludcjr@users.noreply.github.com>
2019-06-14 11:32:58 +0200

    Update calc-circumfrence.js
    The formula for finding the circumference

--- calc-circumfrence.js
+++ calc-circumfrence.js
@@ -1,1 +1,17 @@
 // calcCircumfrence function should be here
+// The formula for finding the circumference
+
+function calcCircumfrence(radius) {
+    /*
+    A value can be assigned as follows to execute in the function.
+    var radius = 5;
+    */
+    var circumference = Math.PI * 2*radius;
+    console.log("The circumference is " + circumference);
+  
+    //radius squared you multiply by itself.
+    var area = Math.PI * radius*radius; 
+    console.log("The area is " + area);
+  }
+
+  calcCircumfrence();
